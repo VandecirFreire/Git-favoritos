dadb122aaea3c3aac4aa924235c483d34a15ac0d
Vandecir <15702156+VandecirFreire@users.noreply.github.com>
2022-07-13 16:52:17 -0300

    JavaScript
    Construindo js

---
+++ js/Favoritos.js
@@ -0,0 +1,85 @@
+// classe que vai conter a lógica dos dados
+// como os dados serão estruturados
+export class Favoritos {
+  constructor(root) {
+    this.root = document.querySelector(root)
+    this.load()
+  }
+
+  load() {
+     this.entries = [
+      {
+        login: 'vandecirfreire',
+        name: 'Vandecir Freire',
+        public_repos: '33',
+        followers: '100'
+      },
+      {
+        login: 'maykbrito',
+        name: 'Mayk Brito',
+        public_repos: '76',
+        followers: '120000'
+      }
+    ]
+  }
+}
+
+// classe que vai criar a visualização e eventos do HTML
+export class FavoritosView extends Favoritos {
+  constructor(root) {
+    super(root)
+
+    this.tbody = this.root.querySelector('table tbody')
+
+    this.update()
+  }
+
+  update() {
+    this.removeAllTr()
+
+    this.entries.forEach( user => {
+      const row = this.createRow()
+
+      row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+      row.querySelector('.user img').alt = `Imagem de ${user.name}`
+      row.querySelector('.user p').textContent = user.name
+      row.querySelector('.user span').textContent = user.login
+      row.querySelector('.repositorios').textContent = user.public_repos
+      row.querySelector('.followers').textContent = user.followers
+
+      this.tbody.append(row)
+    })
+  }
+
+  createRow() {
+    const tr = document.createElement('tr')
+
+    tr.innerHTML = `
+      <td class="user">
+        <img src="https://github.com/vandecirfreire.png" alt="imagem de vandecir freire">
+        <a href="https://github.com/vandecirfreire" target="_blank">
+          <p>Vandecir Freire</p>
+          <span>vandecirfreire</span>
+        </a>
+      </td>
+      <td class="repositorios">
+        33
+      </td>
+      <td class="followers">
+        46
+      </td>
+      <td>
+        <button class="remove">&times;</button>
+      </td>
+    `
+
+    return tr
+  }
+
+  removeAllTr() {
+    this.tbody.querySelectorAll('tr')
+      .forEach((tr) => {
+        tr.remove()
+      })
+  }
+}
